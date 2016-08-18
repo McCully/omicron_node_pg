0b60ee47c444d4ccdbc9f735181af450ac04e2dd
Mat McCully <mccully1894@gmail.com>
2016-08-18 18:19:33 -0500

    little styles

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
 
   // add a book
   $('#book-submit').on('click', postBook);
+  $('#book-list').on('click', '.update', putBook);
+  $('#book-list').on('click', '.delete', deleteBook);
+  $('#filter-submit').on('click', filterBooksByGenre);
 });
 /**
  * Retrieve books from server and append to DOM
@@ -15,11 +18,31 @@ function getBooks() {
       console.log('GET /books returns:', books);
       books.forEach(function (book) {
         var $el = $('<li></li>');
-        $el.append('<strong>' + book.title + '</strong>');
-        $el.append(' <em>' + book.author + '</em');
-        $el.append(' <time>' + book.published + " " + '</time>');
-        $el.append('<em>' + book.edition + ' ' + '</em>');
-        $el.append('<em>' + book.publisher + " " + '</em>');
+
+        var bookProperties = ["title" , "author" , "published" , "publisher" , "edition" , "genre"];
+
+        bookProperties.forEach(function (property) {
+          var inputType = 'text';
+          if (property == 'published') {
+            //inputType = 'date';
+            //get strings for month/day/year
+            book[property] = new Date(book[property]);
+            var month = book[property].getUTCMonth(book[property]) + 1; //months from 1-12
+            var day = book[property].getUTCDate(book[property]);
+            var year = book[property].getUTCFullYear(book[property]);
+
+            //concatenate into one string month/day/year and set to book.published as text
+            book[property] = month + '/' + day + '/' + year;
+          }
+
+          var $input = $('<input type="text" id="' + property + '"name="' + property + '"/>')
+          $input.val(book[property]);
+          $el.append($input);
+        });
+        $el.data('bookId' , book.id);
+        $el.append('<button class = "update">Update</button>');
+        $el.append('<button class = "delete">Delete</button>');
+
         $('#book-list').append($el);
       });
     },
@@ -54,5 +77,96 @@ function postBook() {
     error: function (response) {
       console.log('POST /books does not work...');
     },
+  });
+}
+
+function putBook() {
+  var book = {};
+  var inputs = $(this).parent().children().serializeArray();
+
+  $.each(inputs, function (i, field) {
+    book[field.name] = field.value;
+  });
+  console.log('book we are putting', book);
+
+  var bookId = $(this).parent().data('bookId');
+
+  $.ajax({
+    type: 'PUT',    //update a resource
+    url: '/books/' + bookId,
+    data: book,
+    success: function () {
+      $('#book-list').empty();
+      getBooks();
+    },
+
+    error: function () {
+      console.log('Error PUT /books/' + bookId);
+    },
+  });
+
+}
+
+function filterBooksByGenre() {
+  var filterValue = $('#genres :selected').val();
+
+  $.ajax({
+    type: 'GET',
+    url: '/books/' + filterValue,
+    success: function (books) {
+      console.log('GET /books returns:', books);
+      $('#book-list').empty();
+
+      books.forEach(function (book) {
+        var $el = $('<div></div>');
+
+        var bookProperties = ['title', 'author', 'published', 'edition', 'publisher', 'genre'];
+
+        bookProperties.forEach(function (property) {
+          if (property == 'published') {
+            book[property] = new Date(book[property]);
+
+
+            var month = book[property].getUTCMonth(book[property]) + 1; //months from 1-12
+            var day = book[property].getUTCDate(book[property]);
+            var year = book[property].getUTCFullYear(book[property]);
+
+            
+            book[property] = month + "/" + day + "/" + year;
+          }
+          var $input = $('<input type="text" id="' + property + '" name="' + property + '" />');
+          $input.val(book[property]);
+          $el.append($input);
+        });
+
+        $el.data('bookId', book.id);
+        $el.append('<button class="update">Update</button>');
+        $el.append('<button class="delete">Delete</button>');
+
+        $('#book-list').append($el);
+
+      });
+    },
+
+    error: function (response) {
+      console.log('GET /books fail. No books could be retrieved!');
+    },
+  });
+}
+
+function deleteBook() {
+  var bookId = $(this).parent().data('bookId');
+
+  $.ajax({
+    type: 'DELETE',
+    url: '/books/' + bookId,
+    success: function () {
+      console.log('DELETE success');
+      $('#book-list').empty();
+      getBooks();
+    },
+    error: function () {
+      console.log('DELETE failed');
+    }
   });
 }
--- server/routes/books.js
+++ server/routes/books.js
@@ -8,7 +8,7 @@ router.get('/'  , function(req , res){
   //Retrieve books from database
   pg.connect(connect, function(err, client, done) {
     if (err) {
-      console.log("ERROR1");
+      console.log("ERROR1" , err);
       res.sendStatus(500);
     }
     client.query('SELECT * FROM books' , function(err , result){
@@ -30,17 +30,81 @@ router.post('/' , function(req , res){
       console.log("ERROR3")
       res.sendStatus(500);
     }
-    client.query('INSERT INTO books (author, title , published , edition , publisher)'
-      + 'VALUES ($1 , $2 , $3 , $4 , $5)' , [book.author , book.title , book.published , book.edition , book.publisher],
+    client.query('INSERT INTO books (author, title , published , edition , publisher , genre)'
+      + 'VALUES ($1 , $2 , $3 , $4 , $5 , $6)' , [book.author , book.title , book.published , book.edition , book.publisher , book.genre],
       function (err , result) {
         done();
         if (err) {
-          console.log('ERROR4');
+          console.log('ERROR4' , err);
           res.sendStatus(500);
-        }
+        } else {
         res.sendStatus(201);
+        }
       }
     )
+  });
+});
+
+router.put('/:id', function (req, res) {
+  var id = req.params.id;
+  var book = req.body;
+
+  pg.connect(connect, function (err, client, done) {
+    if (err) {
+      console.log("ERROR5" , err)
+      res.sendStatus(500);
+    }
+
+    client.query('UPDATE books ' + 'SET title = $1, ' + 'author = $2, ' + 'published = $3, ' + 'genre = $4 ' + 'WHERE id = $5', [book.title, book.author, book.published, book.genre, id],
+      function (err, result) {
+        done();
+        if (err) {
+          console.log('err', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+  });
+});
+
+router.delete('/:id', function (req, res) {
+  var id = req.params.id;
+
+  pg.connect(connect, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+
+    client.query('DELETE FROM books ' + 'WHERE id = $1', [id],
+      function (err, result) {
+        done();
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        res.sendStatus(200);
+        });
+  });
+})
+
+router.get('/:id', function(req, res) {
+  var id = req.params.id;
+  console.log('id', id);
+  // Retrieve books from database
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT * FROM books ' + 'WHERE genre = $1', [id], function (err, result) {
+      done();
+      if (err) {
+        res.sendStatus(500);
+      }
+      res.send(result.rows);
+
+    });
   });
 });
 module.exports = router;
